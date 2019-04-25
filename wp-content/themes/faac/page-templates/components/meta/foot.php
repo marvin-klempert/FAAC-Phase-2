71c65d2be9ad3580347cd0a04da4b95472ecf8e7
benjatron <ben@bendavidson.me>
2019-04-25 10:01:38 -0400

    Updated markup

--- wp-content/themes/faac/page-templates/components/meta/foot.php
+++ wp-content/themes/faac/page-templates/components/meta/foot.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * WordPress footer and anything else needed here
+ * WordPress footer
  */
 do_action('get_footer');
 wp_footer();
+
+// instant.page script
 ?>
-<?php // instant.page script ?>
 <script src="//instant.page/1.2.2" type="module" integrity="sha384-2xV8M5griQmzyiY3CDqh1dn4z3llDVqZDqzjzcY+jCBCk/a5fXJmuZ/40JJAPeoU"></script>
--- wp-content/themes/faac/page-templates/homepage.php
+++ wp-content/themes/faac/page-templates/homepage.php
@@ -1,47 +1,179 @@
 <?php
 /**
- * Template Name: General Content
- * Description: General content page
+ * Template Name: Homepage
+ * Description: Layout for the homepage / front page of the website
+ *
+ * This layout consists of:
+ *    Header
+ *    Main body
+ *    Footer
+ *
+ * The header contains:
+ *    Standard navigation
+ *    Hero slider
+ *
+ * The main body consists of:
+ *    Intro content component
+ *    Category heading component
+ *    Category feature component
+ *    Division intro component
+ *    - Uses the intro-content component file
+ *    Division grid component
+ *
+ * The footer contains:
+ *    Video promotion component
+ *    Photo promotion component
+ *    Links promotion component
+ *    General footer layout
  */
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
-  <?php
-  // Header meta tags
-  get_partial('meta', 'head');
-  ?>
-
-  <body <?php body_class('homepage'); ?> ?>
-
+<html <?php language_attributes(); ?> >
+  <head>
     <?php
-    // Google Tag Manager <noscript> option
-    get_partial('meta', 'tag-manager-noscript');
+    // Header meta, scripts, and styles
+    fwd_preload( 'homepage', 'css' );
+    fwd_preload( 'homepage', 'js' );
 
-    // Included for the sake of plugins and child themes that rely on it.
-    do_action('get_header');
+    get_layout( 'header' );
+
+    wp_enqueue_style( 'homepage' );
     ?>
+  </head>
 
+  <body <?php body_class( 'homepage' ); ?>>
     <?php
-    // Website header area
+    // Body open meta and functions
+    get_layout( 'body-open' );
     ?>
     <header class="homepage__header">
+      <section class="homepage__navigation">
+        <?php
+        // General navigation component
+        fwd_query_var( 'logo', 'faac_menuLogo', 'option' );
+        get_component( 'navigation', 'general' );
+        ?>
+      </section>
+
+      <?php
+      // Banner slider component
+      if( get_field( 'homepage_slider_dynamicSlider') ):
+      ?>
+        <section class="homepage__banner">
+          <?php
+          fwd_query_var( 'slider', 'homepage_slider_dynamicSlider');
+          get_component( 'banners', 'slider' );
+          ?>
+        </section>
+      <?php
+      endif;
+      ?>
     </header>
 
-    <?php
-    // Main content area
-    ?>
     <main class="homepage__main">
+      <?php
+      // Intro content component
+      if( get_field( 'homepage_intro' ) ):
+      ?>
+        <section class="homepage__intro">
+          <?php
+          fwd_query_var( 'content', 'homepage_intro' );
+          set_query_var( 'leading', true );
+          get_component( 'general', 'centered-content' );
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Category headings component
+      if( get_field( 'homepage_categoryHeadings_heading' ) ):
+      ?>
+        <section class="homepage__category-headings">
+          <?php
+          fwd_query_var( 'headings', 'homepage_categoryHeadings_heading' );
+          get_component( 'options', 'category-headings' );
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Category feature component
+      if( get_field( 'homepage_categories_categoryFeature') ):
+      ?>
+        <section class="homepage__category-feature">
+          <?php
+          fwd_query_var( 'features', 'homepage_categories_categoryFeature');
+          get_component( 'options', 'category-feature' );
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Division Introduction (uses centered-content)
+      if( get_field( 'homepage_divisionIntro' ) ):
+      ?>
+        <section class="homepage__division-intro">
+          <?php
+          fwd_query_var( 'content', 'homepage_divisionIntro' );
+          get_component( 'general', 'centered-content' );
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Division grid component
+      if( get_field( 'homepage_divisionGrid_divisionGrid' ) ):
+      ?>
+        <section class="homepage__division-grid">
+          <?php
+          fwd_query_var( 'grid', 'homepage_divisionGrid_divisionGrid' );
+          get_component( 'general', 'division-logos' );
+          ?>
+        </section>
+      <?php
+      endif;
+      ?>
     </main>
 
-    <?php
-    // Footer content
-    ?>
     <footer class="homepage__footer">
-    </footer>
+      <?php
+      // Video promotion component
+      if( have_rows('videoPromo_playlist', 'option') ):
+      ?>
+        <section class="404__video-promotion">
+          <?php
+          get_component('promotions', 'video');
+          ?>
+        </section>
+      <?php
+      endif;
 
+      // Photo promotion component
+      if( get_field('photoPromo_background', 'option') ):
+      ?>
+        <section class="404__photo-promotion">
+          <?php
+          get_component('promotions', 'photo');
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Promotional links component
+      ?>
+      <section class="404__promotion-links">
+        <?php
+        get_component('promotions', 'links');
+        ?>
+      </section>
+
+      <?php
+      get_layout( 'footer' );
+      ?>
+    </footer>
     <?php
-    // Footer meta data and scripts. Includes the WP admin footer, etc.
-    get_partial('meta', 'foot');
+    wp_enqueue_script( 'homepage' );
+    get_component( 'meta', 'foot' );
     ?>
   </body>
 </html>
