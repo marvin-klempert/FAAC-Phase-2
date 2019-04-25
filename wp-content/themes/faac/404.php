a9d91140ca3cc5ae2469fb9e885c5f7acffd1381
benjatron <ben@bendavidson.me>
2019-04-25 09:58:39 -0400

    Updated layout

--- wp-content/themes/faac/404.php
+++ wp-content/themes/faac/404.php
@@ -8,16 +8,19 @@
  *    Footer
  *
  * The header contains:
- *    Standard navigation
- *    Hero banner area
- *      The text set for this page is not editable
+ *    General navigation component
+ *    Basic banner component
+ *    - The text set for this page is not editable
  *
  * The main body content consists of:
  *    "Not Found" text block
- *    CTA Sections
  *
  * The footer contains:
- *    Universal footer content
+ *    CTA promotions component
+ *    Video promotions component
+ *    Photo promotions component
+ *    Links promotion component
+ *    General footer layout
  */
 ?>
 <!doctype html>
@@ -28,7 +31,7 @@
     fwd_preload( '404', 'css' );
     fwd_preload( '404', 'js' );
 
-    get_partial('meta', 'head');
+    get_layout( 'header' );
 
     wp_enqueue_style( '404' );
     ?>
@@ -37,14 +40,14 @@
   <body <?php body_class('404'); ?>>
     <?php
     // Body open meta and functions
-    get_partial('meta', 'body-open');
+    get_layout( 'body-open' );
     ?>
     <header class="404__header">
       <section class="404__navigation">
         <?php
         // General navigation
         fwd_query_var( 'logo', 'faac_menuLogo', 'option' );
-        get_partial('navigation', 'general');
+        get_component('navigation', 'general');
         ?>
       </section>
 
@@ -54,8 +57,8 @@
         fwd_query_var( 'background', 'faac_postHeader', 'option' );
         set_query_var( 'category', '' );
         set_query_var( 'upper', '' );
-        set_query_var( 'lower', 'Oops...');
-        get_partial('banners', 'basic');
+        set_query_var( 'lower', 'Oops...' );
+        get_component( 'banners', 'basic' );
         ?>
       </section>
     </header>
@@ -69,63 +72,61 @@
       </div>
     </article>
 
-    <?php
-    // Calls-to-action component
-    if( get_field('cta_tagline', 'option') ):
-      fwd_query_var( 'background', 'cta_background', 'option' );
-      fwd_query_var( 'button', 'cta_buttonText', 'option' );
-      fwd_query_var( 'link', 'cta_pageLink', 'option' );
-      fwd_query_var( 'phone', 'cta_phoneNumber', 'option' );
-      fwd_query_var( 'tagline', 'cta_tagline', 'option' );
-    ?>
-      <section class="404__calls-to-action">
-        <?php
-        get_partial( 'promotions', 'calls-to-action' );
-        ?>
-      </section>
-    <?php
-    endif;
-    ?>
-
-    <?php
-    // Video promotion component
-    if( have_rows('videoPromo_playlist') ):
-    ?>
-      <section class="404__video-promotion">
-        <?php
-        get_partial('promotions', 'video');
-        ?>
-      </section>
-    <?php
-    endif;
-
-    // Photo promotion component
-    if( get_field('photoPromo_background', 'option') ):
-    ?>
-      <section class="404__photo-promotion">
-        <?php
-        get_partial('promotions', 'photo');
-        ?>
-      </section>
-    <?php
-    endif;
-
-    // Promotional links component
-    ?>
-    <section class="404__promotion-links">
-      <?php
-      get_partial('promotions', 'links');
-      ?>
-    </section>
-
     <footer class="404__footer">
       <?php
-      dynamic_sidebar( 'sidebar-footer' );
+      // Calls-to-action component
+      if( get_field('cta_tagline', 'option') ):
+        fwd_query_var( 'background', 'cta_background', 'option' );
+        fwd_query_var( 'button', 'cta_buttonText', 'option' );
+        fwd_query_var( 'link', 'cta_pageLink', 'option' );
+        fwd_query_var( 'phone', 'cta_phoneNumber', 'option' );
+        fwd_query_var( 'tagline', 'cta_tagline', 'option' );
+      ?>
+        <section class="404__calls-to-action">
+          <?php
+          get_component( 'promotions', 'calls-to-action' );
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Video promotion component
+      if( have_rows('videoPromo_playlist', 'option') ):
+      ?>
+        <section class="404__video-promotion">
+          <?php
+          get_component('promotions', 'video');
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Photo promotion component
+      if( get_field('photoPromo_background', 'option') ):
+      ?>
+        <section class="404__photo-promotion">
+          <?php
+          get_component('promotions', 'photo');
+          ?>
+        </section>
+      <?php
+      endif;
+
+      // Promotional links component
+      ?>
+      <section class="404__promotion-links">
+        <?php
+        get_component('promotions', 'links');
+        ?>
+      </section>
+
+      <?php
+      get_layout( 'footer' );
       ?>
     </footer>
     <?php
     wp_enqueue_script( '404' );
-    get_partial( 'meta', 'foot' );
+    get_component( 'meta', 'foot' );
     ?>
   </body>
 
