5ea99804a0186e920e75472ebc0d92562c8cd6df
benjatron <ben@bendavidson.me>
2019-04-25 10:01:57 -0400

    Created page files

---
+++ wp-content/themes/faac/resources/scripts/src/pages/homepage.js
@@ -0,0 +1,10 @@
+// Scripts for the homepage page template
+'use strict'
+
+/**
+ * Imports SCSS for Webpack parsing and sets strict mode
+ */
+require('../../../styles/src/pages/homepage.scss');
+
+// Common JS
+import '../common';
