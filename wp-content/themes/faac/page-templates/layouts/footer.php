a1d1403fb33ec92acf73e65e1c663b9a9f8dcca3
benjatron <ben@bendavidson.me>
2019-04-25 14:47:24 -0400

    Updated component references

--- wp-content/themes/faac/page-templates/layouts/footer.php
+++ wp-content/themes/faac/page-templates/layouts/footer.php
@@ -5,26 +5,26 @@
 
 // Newsletter signup component
 fwd_query_var( 'form', 'footer_newsletterSignup_formID', 'option' );
-get_component( 'widgets', 'newsletter-signup' );
+get_component( 'footer', 'newsletter-signup' );
 
 // Masthead and buttons component
 fwd_query_var( 'logo', 'footer_masthead_logo', 'option' );
 fwd_query_var( 'buttons', 'footer_masthead_buttons', 'option' );
-get_component( 'widgets', 'masthead-buttons' );
+get_component( 'footer', 'masthead-buttons' );
 
 // Navigation menu component
 fwd_query_var( 'menu', 'footer_navigationMenu', 'option' );
-get_component( 'widgets', 'navigation' );
+get_component( 'footer', 'navigation' );
 
 // Division logo component
 fwd_query_var( 'logos', 'footer_divisionLogo_grid', 'option' );
-get_component( 'widgets', 'division-grid' );
+get_component( 'footer', 'division-grid' );
 
 // Contact info component
 fwd_query_var( 'social', 'footer_socialMedia', 'option' );
 fwd_query_var( 'contact', 'footer_contactBlocks', 'option' );
 fwd_query_var( 'parent', 'footer_parentCo', 'option' );
-get_component( 'widgets', 'contact-info' );
+get_component( 'footer', 'contact-info' );
 
 // Copyright component
 fwd_query_var( 'name', 'footer_copyright_name', 'option' );
@@ -32,4 +32,4 @@ fwd_query_var( 'sitemap', 'footer_copyright_sitemap', 'option' );
 fwd_query_var( 'privacy', 'footer_copyright_privacyPolicy', 'option');
 fwd_query_var( 'tos', 'footer_copyright_termsOfService', 'option' );
 fwd_query_var( 'design', 'footer_copyright_designCredit', 'option' );
-get_component( 'widgets', 'copyright' );
+get_component( 'footer', 'copyright' );
